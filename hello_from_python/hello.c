7f3e1c2e3f47399085e08bad522e6b4a66226c0e
charith jayarangana <cj.ptsz@yahoo.com>
2024-08-13 07:46:12 +0530

    feat: create python function with parameter and call it from c code

--- hello_from_python/hello.c
+++ hello_from_python/hello.c
@@ -1,4 +1,5 @@
 #include <Python.h>
+#include "linebreaker.h"
 
 int main(){
 	// initialize python interpreter
@@ -15,13 +16,42 @@ int main(){
 
 		if(pFunc && PyCallable_Check(pFunc)) {
 			// call the python function
-			PyObject* pValue = PyObject_CallObject(pFunc, NULL);
-			if (pValue != NULL) {
-				printf("Result of Python function: %ld\n",PyLong_AsLong(pValue));
-				Py_DECREF(pValue);
+			printf("calling hello function of python hello_module\n");
+			PyObject* pRetValue1 = PyObject_CallObject(pFunc, NULL);
+			if (pRetValue1 != NULL) {
+				printf("Result of Python function 1: %ld\n",PyLong_AsLong(pRetValue1));
+				Py_DECREF(pRetValue1);
+			} else{
+				printf("Failed to execute hello function\n");
 			}
 		}
 		Py_XDECREF(pFunc);
+
+		line_break(20);
+
+		// get another function from the module
+		PyObject* pFunc_name = PyObject_GetAttrString(pModule, "hello_name");
+
+		// define arguments
+		PyObject* pArgs = PyTuple_Pack(1, PyUnicode_FromString("CJ-404"));
+
+		if (pFunc_name && PyCallable_Check(pFunc_name)) {
+			// call to the function
+			printf("calling hello_name(name) function of python hello_module\n");
+			PyObject* pRetValue2 = PyObject_CallObject(pFunc_name, pArgs);
+
+			if (pRetValue2 == NULL) {
+				// print error
+				printf("Failed to execute hello_name function\n");
+				PyErr_Print();
+			} else {
+				printf("hello_name function called successfully: void result\n");
+				Py_DECREF(pRetValue2);
+			}
+		}
+		Py_XDECREF(pFunc_name);
+
+		
 		Py_DECREF(pModule);
 	}
 
